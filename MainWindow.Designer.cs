cfc1ac92ec5857bebd3dfaa7f7f8feb26160b751
jayouimet <jayouimet@hotmail.com>
2023-03-05 00:31:46 -0500

    Added comments and a bit of a better UI

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -31,13 +31,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnBrowse = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pathListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(387, 23);
+            this.textBox1.Size = new System.Drawing.Size(817, 23);
             this.textBox1.TabIndex = 0;
             // 
             // openFileDialog1
@@ -46,7 +48,7 @@
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(405, 12);
+            this.btnBrowse.Location = new System.Drawing.Point(835, 12);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
             this.btnBrowse.TabIndex = 1;
@@ -54,13 +56,39 @@
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(835, 156);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pathListBox
+            // 
+            this.pathListBox.FormattingEnabled = true;
+            this.pathListBox.ItemHeight = 15;
+            this.pathListBox.Location = new System.Drawing.Point(12, 41);
+            this.pathListBox.Name = "pathListBox";
+            this.pathListBox.Size = new System.Drawing.Size(898, 109);
+            this.pathListBox.TabIndex = 3;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 50);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(922, 187);
+            this.Controls.Add(this.pathListBox);
+            this.Controls.Add(this.startButton);
             this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainWindow";
             this.Text = "KSP2 Fix Corrupted Save";
             this.ResumeLayout(false);
@@ -73,5 +101,7 @@
         private TextBox textBox1;
         private OpenFileDialog openFileDialog1;
         private Button btnBrowse;
+        private Button startButton;
+        private ListBox pathListBox;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -7,7 +7,12 @@ namespace KSP2SaveFixer
 {
     public partial class MainWindow : Form
     {
+        // Will hold the path to the folder to open by default
         public string pathToSaveFiles;
+        // Will hold the selected save files paths
+        public List<string> pathToSaveFilesList;
+        // Will hold the selected save files names
+        public List<string> nameSaveFilesList;
 
         public MainWindow()
         {
@@ -20,93 +25,137 @@ namespace KSP2SaveFixer
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
+            // Initializing the file system dialog box
             this.openFileDialog1.Multiselect = true;
             this.openFileDialog1.Filter = "Json files (*.json)|*.json";
 
-            if (Directory.Exists(this.pathToSaveFiles))
+            // If the directory described in the text box exists, use this path
+            if (Directory.Exists(this.textBox1.Text))
             {
                 this.openFileDialog1.InitialDirectory = this.textBox1.Text;
             }
             else
             {
+                // If not, try to open the default save files path
                 if (Directory.Exists(this.pathToSaveFiles))
                 {
                     this.openFileDialog1.InitialDirectory = this.pathToSaveFiles;
                 }
+                // Otherwise, we do not give a default path. This is defaulted to user -> Documents
             }
 
+            // Show the dialog and if the result was OK
             if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                for (int i = 0; i < this.openFileDialog1.FileNames.Length; i++)
+                // Save the file names + paths to the above list
+                this.pathToSaveFilesList = this.openFileDialog1.FileNames.ToList();
+                this.nameSaveFilesList = this.openFileDialog1.SafeFileNames.ToList();
+                this.updateUI();
+            }
+        }
+
+        private void updateUI()
+        {
+            this.pathListBox.Items.Clear();
+            this.pathListBox.Items.AddRange(pathToSaveFilesList.ToArray());
+            this.startButton.Enabled = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // In case no files were selected, we return
+            if (this.pathToSaveFilesList is null)
+                return;
+
+            // For every file
+            for (int i = 0; i < this.pathToSaveFilesList.Count(); i++)
+            {
+                // Get the file name and path
+                string file = this.pathToSaveFilesList[i];
+                string safeFileName = this.nameSaveFilesList[i];
+
+                try
                 {
-                    string file = this.openFileDialog1.FileNames[i];
-                    string safeFileName = this.openFileDialog1.SafeFileNames[i];
-                    try
+                    // Try to read the file
+                    string json = File.ReadAllText(file);
+                    bool jsonChanged = false;
+                    // Deserialize the file into a JObject
+                    var values = JsonConvert.DeserializeObject(json);
+
+                    // If not null continue
+                    if (values is not null)
                     {
-                        string json = File.ReadAllText(file);
-                        bool jsonChanged = false;
-                        var values = JsonConvert.DeserializeObject(json);
-
-                        if (values is not null)
+                        // Cast into JObject
+                        JObject obj = (JObject)values;
+                        // Get value for attribut Vessels
+                        var vessels = obj["Vessels"];
+                        // For each vessels
+                        foreach (var vessel in vessels)
                         {
-                            JObject obj = (JObject)values;
-                            var vessels = obj["Vessels"];
-                            foreach (var vessel in vessels)
-                            {
-                                try
-                                {
-                                    string currentControlOwnerPartGuid = vessel["vesselState"]["CurrentControlOwnerPart"]["Guid"].ToString();
-
-                                    if (!currentControlOwnerPartGuid.Equals("00000000-0000-0000-0000-000000000000"))
-                                    {
-                                        var parts = vessel["parts"];
-
-                                        bool partIsInVessel = false;
-
-                                        foreach (var part in parts)
-                                        {
-                                            if (part["PartGuid"]["Guid"].ToString().Equals(currentControlOwnerPartGuid))
-                                            {
-                                                partIsInVessel = true;
-                                                break;
-                                            }
-                                        }
-
-                                        if (!partIsInVessel)
-                                        {
-                                            vessel["vesselState"]["CurrentControlOwnerPart"]["Guid"] = "00000000-0000-0000-0000-000000000000";
-                                            jsonChanged = true;
-                                        }
-                                    }
-                                }
-                                // In case an attribute is missing, just move along. We only care about CurrentControlOwnerPart
-                                catch { }
-                            }
-
                             try
                             {
-                                if (jsonChanged)
+                                // Try to get the CurrentControlOwnerPart Guid
+                                string currentControlOwnerPartGuid = vessel["vesselState"]["CurrentControlOwnerPart"]["Guid"].ToString();
+
+                                // If it is not already resetted
+                                if (!currentControlOwnerPartGuid.Equals("00000000-0000-0000-0000-000000000000"))
                                 {
-                                    // Dump the json
-                                    string output = JsonConvert.SerializeObject(values, Newtonsoft.Json.Formatting.Indented);
-                                    File.WriteAllText(file, output);
-                                    MessageBox.Show(safeFileName + "\nResetted part ownership for invalid vessels.");
-                                }
-                                else
-                                {
-                                    MessageBox.Show(safeFileName + "\nNo inconsistencies found.");
+                                    var parts = vessel["parts"];
+
+                                    bool partIsInVessel = false;
+
+                                    // Itterate through parts
+                                    foreach (var part in parts)
+                                    {
+                                        // If we find the CurrentControlOwnerPart Guid, we set the boolean to true and we break the loop
+                                        if (part["PartGuid"]["Guid"].ToString().Equals(currentControlOwnerPartGuid))
+                                        {
+                                            partIsInVessel = true;
+                                            break;
+                                        }
+                                    }
+
+                                    // If the part was not found, we reset the CurrentControlOwnerPart Guid
+                                    if (!partIsInVessel)
+                                    {
+                                        vessel["vesselState"]["CurrentControlOwnerPart"]["Guid"] = "00000000-0000-0000-0000-000000000000";
+                                        // Since a change occured, we keep in mind we need to save the json
+                                        jsonChanged = true;
+                                    }
                                 }
                             }
-                            catch
+                            // In case an attribute is missing, just move along. We only care about CurrentControlOwnerPart
+                            catch { }
+                        }
+
+                        try
+                        {
+                            // If the json has changed, we try to dump it
+                            if (jsonChanged)
                             {
-                                MessageBox.Show(safeFileName + "\nCould not overwrite file, skipping.");
+                                // Dump the json
+                                string output = JsonConvert.SerializeObject(values, Newtonsoft.Json.Formatting.Indented);
+                                File.WriteAllText(file, output);
+                                // Show feedback
+                                MessageBox.Show(safeFileName + "\nResetted part ownership for invalid vessels.");
+                            }
+                            else
+                            {
+                                // Show feedback
+                                MessageBox.Show(safeFileName + "\nNo inconsistencies found.");
                             }
                         }
+                        catch
+                        {
+                            // Show feedback if an error occured, could be for many reasons: No write permissions, file is in use, etc
+                            MessageBox.Show(safeFileName + "\nCould not overwrite file, skipping.");
+                        }
                     }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Could not open file located at : " + file);
-                    }
+                }
+                // Show feedback if an error occured, could be for many reasons: No read permissions, file is deleted, etc
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not open file located at : " + file);
                 }
             }
         }
